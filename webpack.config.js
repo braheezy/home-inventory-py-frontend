58e9251eab4457c02f8c1bea4f1f88889f801415
Michael Braha <michael.c.braha@gmail.com>
2021-10-06 19:44:29 -0700

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const resolve = require("path").resolve;
+
+module.exports = {
+  mode: "production",
+  // When debugging, allow error to be found in source code
+  devtool: "eval-source-map",
+  // entry file to figure out dependencies from
+  entry: __dirname + "/js/index.jsx",
+  // For webpacks HMR
+  devServer: {
+    contentBase: resolve(__dirname, "../public"),
+    publicPath: resolve("../public"),
+    watchContentBase: true,
+    compress: true,
+    port: 9000,
+  },
+  // where webpack will dump built assets
+  output: {
+    // local path on disk
+    path: resolve("../public"),
+    filename: "bundle.js",
+    // path in browser e.g. /server/public
+    publicPath: resolve("../public"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".css"],
+  },
+
+  // TODO: move to .babelrc in static/
+  module: {
+    rules: [
+      {
+        // Babel loaded for JSX -> JS
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    node: "current",
+                  },
+                },
+              ],
+              ["@babel/preset-react"],
+            ],
+            plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
+          },
+        },
+      },
+      {
+        // handle css stylesheets
+        test: /\.csv$/,
+        loader: "style-loader!css-loader?modules",
+      },
+    ],
+  },
+};
