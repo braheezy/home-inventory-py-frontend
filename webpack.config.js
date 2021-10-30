1ccdc3c904e468a0803692dafe3bd6d1731a5737
Michael Braha <michael.c.braha@gmail.com>
2021-10-30 14:04:44 -0700

    refactor per online tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 const resolve = require("path").resolve;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
   // When debugging, allow error to be found in source code
   devtool: "eval-source-map",
   // entry file to figure out dependencies from
-  entry: __dirname + "/js/index.jsx",
+  entry: "./src/index.jsx",
   // For webpacks HMR
   devServer: {
-    contentBase: resolve(__dirname, "./public"),
-    publicPath: resolve("./public"),
+    contentBase: resolve(__dirname, "./dist"),
+    publicPath: resolve("./dist"),
     watchContentBase: true,
     compress: true,
     port: 9000,
+    overlay: true,
+    hot: true,
   },
   // where webpack will dump built assets
   output: {
     // local path on disk
-    path: resolve("./public"),
+    path: resolve("./dist"),
     filename: "bundle.js",
-    // path in browser e.g. /server/public
-    publicPath: resolve("./public"),
+    // path in browser e.g. /server/dist
+    publicPath: resolve("./dist"),
   },
   resolve: {
     extensions: [".js", ".jsx", ".css"],
@@ -59,4 +63,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: ["index.html"],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
